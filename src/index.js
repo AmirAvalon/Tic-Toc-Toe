f459ef0525c687bc64467417b4440d0aad2e1bde
unknown <praktica98@gmail.com>
2020-12-08 11:52:56 +0330

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDom, { render } from "react-dom"
+import Game from "./components/Game"
+import {Grid } from '@material-ui/core'
+import "bootstrap/dist/css/bootstrap.css"
+
+
+ReactDom.render(<Game />, document.getElementById("root"));
